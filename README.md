65c9dbfd493ce7f3c0593c5f9d3b8def44f3d9e5
Enrique Gómez Cruz <enriquegomezcruz@gmail.com>
2018-10-10 21:01:35 -0500

    updated notebooks for julia v1.0

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+**All notebooks were tested on Julia v1.0.**
+
+Plotting is done with gnuplot and its epslatex terminal. A minimal $\LaTeX$ installation with **epstopdf** package will do. If this package is missing and you're in a linux environment then just do the following.
+```bash
+sudo tlmgr update --self
+sudo tlmgr install epstopdf
+```
+
+Also, to display the images directly on the notebook we are converting the tex output from the gnuplot terminal to a png image. We are using the common unix tools: **dvips** and **ghostscript**.
+
 # Table of Contents
 
 - **Ising.ipynb**: Ising 2D model on a rectangular, hexagonal and triangular lattice.
--- gnuplot.jl
+++ gnuplot.jl
@@ -1,49 +1,22 @@
-extension(path::String) = match(r"\.[A-Za-z0-9]+$", path).match
-    
-struct Gnuplot
-    filename::String
-    ext::String
-    f::Pipe
-    p::Base.Process
-    function Gnuplot(filename::String, x::Int, y::Int)
-        f,p = open(pipeline(`gnuplot`,stdout=DevNull, stderr=DevNull),"w")
-        ext = extension(filename)
-        if ext == ".svg"
-            write(f,"set terminal epslatex standalone size $(x)cm,$(y)cm\\
-                    color background 'white' lw 3 font ',12'\\
-                    header '\\usepackage[utf8]{inputenc} \\usepackage{amsmath}';
-                    set output 'gptemp.tex'; set colorsequence podo;")
-        elseif ext == ".png"
-            write(f,"set terminal epslatex standalone size $(x)cm,$(y)cm\\
-                    color background 'white' lw 3 font ',20'\\
-                    header '\\usepackage[utf8]{inputenc} \\usepackage{amsmath}';
-                    set output 'gptemp.tex'; set colorsequence podo;")
-        elseif ext == ".gif"
-            write(f,"set terminal gif animate delay 100;
-                    set output '$filename';
-                    set colorsequence podo;")
-        end
-        return new(filename,ext,f,p)
-    end
+function gnuplot(x::Int, y::Int)
+    process = open(pipeline(`gnuplot`,stdout=devnull, stderr=devnull),"w")
+    print(process,"set terminal epslatex standalone size $(x)cm,$(y)cm\\
+            color background 'white' lw 3 font ',20'\\
+            header '\\usepackage[utf8]{inputenc} \\usepackage{amsmath}';
+            set output 'gptemp.tex';
+            set colorsequence podo;")
+    return process
 end
 
-function plot(gp::Gnuplot)
-    close(gp.f)
-    wait(gp.p.closenotify)
-    if gp.ext == ".svg"
-        run(pipeline(`latex gptemp.tex`, stdout=DevNull, stderr=DevNull))
-        run(pipeline(`dvips gptemp.dvi`, stdout=DevNull, stderr=DevNull))
-        run(pipeline(`gs -dBATCH -dSAFER -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="gptemp.pdf" gptemp.ps`,stdout=DevNull,stderr=DevNull))
-        run(`pdf2svg gptemp.pdf $(gp.filename)`)
-        run(`find -x . -name gptemp\* -delete`)
-        display("image/svg+xml",readstring("./$(gp.filename)"))
-    elseif gp.ext == ".png"
-        run(pipeline(`latex gptemp.tex`, stdout=DevNull, stderr=DevNull))
-        run(pipeline(`dvips gptemp.dvi`, stdout=DevNull, stderr=DevNull))
-        run(pipeline(`gs -dBATCH -dSAFER -dNOPAUSE -sDEVICE=pngalpha -sOutputFile="$(gp.filename)" gptemp.ps`,stdout=DevNull,stderr=DevNull))
-        run(`find -x . -name gptemp\* -delete`)
-        display("image/png",read("./$(gp.filename)"))
-    elseif gp.ext == ".gif"
-        display("image/gif",read("./$(gp.filename)"))          
-    end
+function plot(p::Base.Process)
+    close(p.in)
+    close(p.out)
+    close(p.err)
+    wait(p.closenotify)
+    run(pipeline(`latex gptemp.tex`, stdout=devnull, stderr=devnull))
+    run(pipeline(`dvips gptemp.dvi`, stdout=devnull, stderr=devnull))
+    run(pipeline(`gs -dBATCH -dSAFER -dNOPAUSE -sDEVICE=pngalpha
+            -sOutputFile="output.png" gptemp.ps`,stdout=devnull,stderr=devnull))
+    run(`find -x . -name gptemp\* -delete`)
+    display("image/png",read("output.png"))
 end
